5d4fbced4153e954d9d33bdc64ea5d977869df6f
Ali Andac <ali@millimetric.io>
2022-06-01 01:10:05 +0300

    inits fe

---
+++ ecommerce-fe/src/components/AppHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <CHeader position="sticky" class="mb-4">
+    <CContainer fluid>
+      <CHeaderNav class="d-none d-md-flex me-auto">
+        <CNavItem>
+          <CNavLink href="/">Products</CNavLink>
+        </CNavItem>
+      </CHeaderNav>
+      <CHeaderNav>
+        <CNavItem>
+          <CNavLink href="#">
+            <CIcon class="mx-2" icon="cil-bell" size="lg" />
+          </CNavLink>
+        </CNavItem>
+        <AppHeaderDropdownAccnt />
+      </CHeaderNav>
+    </CContainer>
+    <CHeaderDivider />
+    <CContainer fluid>
+      <AppBreadcrumb />
+    </CContainer>
+  </CHeader>
+</template>
+
+<script>
+import AppBreadcrumb from './AppBreadcrumb'
+import AppHeaderDropdownAccnt from './AppHeaderDropdownAccnt'
+
+export default {
+  name: 'AppHeader',
+  components: {
+    AppBreadcrumb,
+    AppHeaderDropdownAccnt,
+  },
+}
+</script>
---
+++ ecommerce-fe/src/views/Products.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <CRow>
+      <CCol>
+        <CCard class="mb-4">
+          <CCardHeader> Ecommerce App Products</CCardHeader>
+          <CCardBody> Products will be listed in here </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+  </div>
+</template>
